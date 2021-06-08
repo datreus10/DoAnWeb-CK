2fc8c219603692e268b5d86a264719dd61d8095c
dat <thanhdatvn11@gmail.com>
2021-06-08 10:26:41 +0700

    clean code

--- controllers/customer/customer.js
+++ controllers/customer/customer.js
@@ -1,24 +1,29 @@
 const User = require('../../model/user.js')
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
 const getCustomer = async (req, res) => {
     try {
-        
-        const id=req.userID;
-        if(id){
+
+        const id = req.userID;
+        if (id) {
             const UserID = await User.findById(id);
             const Username = UserID.name;
             const email = UserID.email;
-            if(UserID) 
-                return res.status(201).render('./customer', {Username: Username, email: email});
-        }
-        else
-        {
+            if (UserID)
+                return res.status(201).render('./customer', {
+                    Username: Username,
+                    email: email
+                });
+        } else {
             return res.status(401).redirect('/');
         }
 
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(409).json({
+            message: error.message
+        });
     }
 }
-module.exports = {getCustomer}
+module.exports = {
+    getCustomer
+}
