73b3bf2d6cdad79ddbbb65dbb955d7b799cc8c85
dat <thanhdatvn11@gmail.com>
2021-06-22 22:07:11 +0700

    clean code

--- route/admin/index.js
+++ route/admin/index.js
@@ -4,24 +4,27 @@ const Product = require('../../model/product');
 const Cart = require('../../model/cart')
 const Bill = require('../../model/bill')
 const jwt = require('jsonwebtoken');
-const {auth_admin} = require ('../../middleware/auth_admin')
-router.get('/', auth_admin,async (req, res) => {
-                    res.render("./admin/index", {
-                        products: await Product.find().sort({
-                            createAt: -1
-                        }).limit(10),
-                    })
-                }
-)
-router.get('/test',async (req,res)=>{
-   
-    data = await Cart.find({
-        'items.itemId':'60b9ff89b5f3622a04a3420f'
+const {
+    auth_admin
+} = require('../../middleware/auth_admin')
+
+
+router.get('/', auth_admin, async (req, res) => {
+    res.render("./admin/index", {
+        products: await Product.find().sort({
+            createAt: -1
+        }).limit(10),
+    })
+})
+router.get('/test', async (req, res) => {
+
+    let data = await Cart.find({
+        'items.itemId': '60b9ff89b5f3622a04a3420f'
     }).populate({
         path: 'items.itemId',
-      
+
     })
-  
+
     res.send({
         data
     })
